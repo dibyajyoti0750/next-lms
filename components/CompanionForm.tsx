4a3e31466d747b97087fea3b0588baa5e0a0e060
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2026-05-03 11:28:18 +0530

    connect supabase

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -22,6 +22,8 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "@/components/ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, "Companion is required."),
@@ -45,9 +47,15 @@ export default function CompanionForm() {
     },
   });
 
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    // Do something with the form values.
-    console.log(data);
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    const companion = await createCompanion(data);
+
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create a companion");
+      redirect("/");
+    }
   };
 
   return (
